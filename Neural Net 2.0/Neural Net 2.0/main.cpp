bc1e33977f0248d1e2e7bf64709c290adf615dcb
Ryan Krol <ryankr@hotmail.co.uk>
2017-05-19 22:59:56 +0100

    New Attempt

---
+++ Neural Net 2.0/Neural Net 2.0/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Neural Net 2.0
+//
+//  Created by Ryan Krol on 19/05/2017.
+//  Copyright © 2017 Ryan Krol. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
